4f4aadb7e7f3ed1536b9277903d59921626b41bc
Nikolai <35627380+destbg@users.noreply.github.com>
2021-01-20 16:21:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,41 +15,36 @@ app.use(express.static(path.join(__dirname, "client")));
 const streams = [];
 
 io.on("connection", (sock) => {
-  const id = sock.id;
-  let peer;
-
-  sock.on("disconnect", () => {
-    const index = streams.findIndex((f) => f.id == id);
-    if (index != -1) {
-      streams.splice(index, 1);
-    }
-    peer.end();
-  });
-
-  sock.on("is-stream", () => {
-    streams.push({ id: id, peer: peer });
-  });
-
-  sock.on("signal", (data) => {
-    peer = new SimplePeer({ trickle: false, wrtc: wrtc });
-    peer.on("signal", (data) => {
-      sock.emit("send-signal", JSON.stringify(data));
-    });
-    peer.signal(data);
-  });
-
-  sock.on("join-stream", (stream) => {
-    const index = streams.findIndex((f) => f.id == stream);
-    const clientPeer = streams[index];
-    if (clientPeer) {
-      const audioTrack = clientPeer._pc.addTransceiver("audio").receiver.track;
-      const videoTrack = clientPeer._pc.addTransceiver("video").receiver.track;
-      const audioTransceiver = peer._pc.addTransceiver("audio");
-      const videoTransceiver = peer._pc.addTransceiver("video");
-      audioTransceiver.sender.replaceTrack(audioTrack);
-      videoTransceiver.sender.replaceTrack(videoTrack);
-    }
-  });
+  // const id = sock.id;
+  // const peer = new SimplePeer({ trickle: false, wrtc: wrtc });
+  // peer.on("signal", (data) => {
+  //   sock.emit("send-signal", JSON.stringify(data));
+  // });
+  // sock.on("disconnect", () => {
+  //   const index = streams.findIndex((f) => f.id == id);
+  //   if (index != -1) {
+  //     streams.splice(index, 1);
+  //   }
+  //   peer.end();
+  // });
+  // sock.on("is-stream", () => {
+  //   streams.push({ id: id, peer: peer });
+  // });
+  // sock.on("signal", (data) => {
+  //   peer.signal(data);
+  // });
+  // sock.on("join-stream", (stream) => {
+  //   const index = streams.findIndex((f) => f.id == stream);
+  //   const clientPeer = streams[index];
+  //   if (clientPeer) {
+  //     const audioTrack = clientPeer._pc.addTransceiver("audio").receiver.track;
+  //     const videoTrack = clientPeer._pc.addTransceiver("video").receiver.track;
+  //     const audioTransceiver = peer._pc.addTransceiver("audio");
+  //     const videoTransceiver = peer._pc.addTransceiver("video");
+  //     audioTransceiver.sender.replaceTrack(audioTrack);
+  //     videoTransceiver.sender.replaceTrack(videoTrack);
+  //   }
+  // });
 });
 
 app.get("/streams", (_, res) => {
