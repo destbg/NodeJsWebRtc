3f2c14570878a3c181c9da621d6de4671cfe5160
Nikolai <35627380+destbg@users.noreply.github.com>
2021-06-14 15:37:38 +0300

    Change of logic

--- client/script/index.js
+++ client/script/index.js
@@ -1,28 +1,26 @@
-(async () => {
-  const otherStreams = document.getElementById("other-streams");
-  const reloadStreams = document.getElementById("reload-streams");
+const otherStreams = document.getElementById("other-streams");
+const reloadStreams = document.getElementById("reload-streams");
 
-  reloadStreams.addEventListener("click", () => getStreams());
+reloadStreams.addEventListener("click", () => getStreams());
 
-  getStreams();
+getStreams();
 
-  async function getStreams() {
-    otherStreams.innerHTML = "";
-    const streamsResult = await fetch("/streams").then((data) => data.json());
+async function getStreams() {
+  otherStreams.innerHTML = "";
+  const streamsResult = await fetch("/streams").then((data) => data.json());
 
-    for (const stream of streamsResult.streams) {
-      const button = document.createElement("div");
-      button.classList.add("stream-link");
-      button.innerHTML = stream;
-      button.addEventListener("click", () => {
-        joinStream(stream);
-      });
+  for (const stream of streamsResult.streams) {
+    const button = document.createElement("div");
+    button.classList.add("stream-link");
+    button.innerHTML = stream;
+    button.addEventListener("click", () => {
+      joinStream(stream);
+    });
 
-      otherStreams.appendChild(button);
-    }
+    otherStreams.appendChild(button);
   }
+}
 
-  function joinStream(stream) {
-    window.location.href = "/stream?id=" + stream;
-  }
-})();
+function joinStream(stream) {
+  window.location.href = "/stream?id=" + stream;
+}
